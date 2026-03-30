7b529c3f393e2865b784d150c251f98974319dbe
JayPiGB <jp.borgneth@gmail.com>
2026-03-30 15:17:59 -0300

    fix attack animation playing both sections

--- Source/UE5_CombatArena/Private/Items/Weapons/Weapon.cpp
+++ Source/UE5_CombatArena/Private/Items/Weapons/Weapon.cpp
@@ -76,7 +76,7 @@ void AWeapon::OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* Oth
 		ETraceTypeQuery::TraceTypeQuery1,
 		false,
 		ActorsToIgnore,
-		EDrawDebugTrace::ForDuration,
+		EDrawDebugTrace::None,
 		BoxHitResult,
 		true
 	);
